756f145837ce119964243e42c6e0d76c531d2079
minng <wjsalsrud02@gmail.com>
2025-09-16 17:52:44 +0900

    20250916

--- js/book.js
+++ js/book.js
@@ -22,8 +22,8 @@ async function fetchBooks(query) {
 
 
 async function bookData() {
-    try {
-        const querys = ["자바스크립트", '강아지', '정원', '파리', '경제','사과','기욤뮈소', '박완서','사랑'];
+try {
+        const querys = ["꽃", '강아지', '정원', '파리', '경제','사과','기욤뮈소', '박완서','사랑'];
 
         for (let i=0; i<querys.length; i++) {
             const data = await fetchBooks(querys[i]);
@@ -38,21 +38,63 @@ async function bookData() {
 
             if (!doc) return; // 데이터가 부족할 경우 생략
 
-            // <img>
-            const img = document.createElement("img");
-            img.src = doc.thumbnail;
-            boxElements[i].appendChild(img);
+            boxElements[i].innerHTML=`<img src="${doc.thumbnail}">
+            <div class="text2">
+              <h3><a href="#">${doc.title}</a><br><span>${doc.price}</span></h3>
+            </div>`
+        }
 
-            // // <h3> 제목
-            // const h3 = document.createElement("h3");
-            // h3.textContent = doc.title;
-            // boxElements[i].appendChild(h3);
+    } catch (error) {
+        console.log('에러발생', error);
+    }
 
-            // // <h6> 저자
-            // const h6 = document.createElement("h6");
-            // h6.textContent = doc.authors;
-            // boxElements[i].appendChild(h6);
-          
+
+try {
+        const querys = ["자바스크립트", '강아지', '정원', '파리', '경제','사과','기욤뮈소', '박완서','사랑'];
+
+        for (let i=0; i<querys.length; i++) {
+            const data = await fetchBooks(querys[i]);
+            console.log(data);
+
+            // .box 요소 전체 선택
+            const boxElements = document.querySelectorAll(".new_item2 .swiper-slide");
+            console.log(boxElements)
+
+            // documents 데이터를 각 box에 대응하여 렌더링            
+            const doc = data.documents[0];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            boxElements[i].innerHTML=`<img src="${doc.thumbnail}">
+            <div class="text2">
+              <h3><a href="#">${doc.title}</a><br><span>${doc.price}</span></h3>
+            </div>`
+        }
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+
+    try {
+        const querys = ["꽃", '영화', '정원', '한강', '경제','사과','미술', '박완서','동화'];
+
+        for (let i=0; i<querys.length; i++) {
+            const data = await fetchBooks(querys[i]);
+            console.log(data);
+
+            // .box 요소 전체 선택
+            const boxElements = document.querySelectorAll(".mySwiper2 .swiper-slide");
+            console.log(boxElements)
+
+            // documents 데이터를 각 box에 대응하여 렌더링            
+            const doc = data.documents[1];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            boxElements[i].innerHTML=`<img src="${doc.thumbnail}">
+            <div class="text2">
+              <h3><a href="#">${doc.title}</a><br><span>${doc.price}</span></h3>
+            </div>`
         }
 
     } catch (error) {
@@ -66,11 +108,85 @@ try {
             console.log(data);
 
             // .box 요소 전체 선택
-            const boxElements = document.querySelectorAll(".new_item .swiper-slide");
+            const boxElements = document.querySelectorAll(".mySwiper4 .swiper-slide");
             console.log(boxElements)
 
             // documents 데이터를 각 box에 대응하여 렌더링            
-            const doc = data.documents[0];
+            const doc = data.documents[2];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            boxElements[i].appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title;
+            boxElements[i].appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            boxElements[i].appendChild(h6);
+          
+        }
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+
+    try {
+        const querys = ["자바스크립트", '강아지', '정원', '파리', '경제','사과','기욤뮈소', '박완서','사랑'];
+
+        for (let i=0; i<querys.length; i++) {
+            const data = await fetchBooks(querys[i]);
+            console.log(data);
+
+            // .box 요소 전체 선택
+            const boxElements = document.querySelectorAll(".mySwiper5 .swiper-slide");
+            console.log(boxElements)
+
+            // documents 데이터를 각 box에 대응하여 렌더링            
+            const doc = data.documents[3];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            boxElements[i].appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title;
+            boxElements[i].appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            boxElements[i].appendChild(h6);
+          
+        }
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+    
+    try {
+        const querys = ["자바스크립트", '강아지', '정원', '파리', '경제','사과','기욤뮈소', '박완서','사랑'];
+
+        for (let i=0; i<querys.length; i++) {
+            const data = await fetchBooks(querys[i]);
+            console.log(data);
+
+            // .box 요소 전체 선택
+            const boxElements = document.querySelectorAll(".mySwiper6 .swiper-slide");
+            console.log(boxElements)
+
+            // documents 데이터를 각 box에 대응하여 렌더링            
+            const doc = data.documents[3];
 
             if (!doc) return; // 데이터가 부족할 경우 생략
 
@@ -96,6 +212,7 @@ try {
     }
 
 }
+
 
 
 
